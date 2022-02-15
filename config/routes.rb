151060ee428e688fd14f1f31cd95315b000ba9b0
おみおみ <github-omiomi@example.com>
2022-02-15 22:27:32 +0900

    Make post functions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root "homes#index"
+  root "posts#index"
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_135145) do
+ActiveRecord::Schema.define(version: 2022_02_05_155318) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "post_content"
+    t.string "address"
+    t.integer "counts_good", default: 0
+    t.integer "thread_id", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
