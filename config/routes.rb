0104e92788ff2794b981555f1c8ca1e9da7f3b6b
おみおみ <github-omiomi@example.com>
2022-03-08 20:37:24 +0900

    make post detail

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "posts#index"
+  get "posts/detail/:id" => "posts#detail"
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
