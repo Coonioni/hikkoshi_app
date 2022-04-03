062a461825e8f09fcbbd74e89944b98e12c7de40
おみおみ <github-omiomi@example.com>
2022-04-03 15:30:43 +0900

    Insert data into mysqlDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 require 'csv'
+# coding: utf-8
 
 CSV.foreach('db/data/station_name.csv', headers: true) do |row|
   Tag.create!(
-    tag_name: row['station_name']
+    :tag_name => row[0]
   )
 end
