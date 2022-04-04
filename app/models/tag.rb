4beef6ff6234c6bca2096001971f74393310bfc5
おみおみ <github-omiomi@example.com>
2022-04-05 00:14:25 +0900

    add tag test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,11 @@
 class Tag < ApplicationRecord
 
-  validates :tag_name, uniqueness: true
-
   has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
+
+  validates :tag_name, uniqueness: { case_sensitive: true },
+                        presence: true,
+                        length: { maximum: 15 },
+                        length: { minimum: 1 }
+
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
+  
   setup do
-    @tag = Tag.new(id: 1,tag_name: "Tokyo")
+    @tag = Tag.new(id: 1,tag_name: "東京")
   end
 
   test "Tag is uniquness true" do
@@ -10,4 +11,29 @@ class TagTest < ActiveSupport::TestCase
     @tag.save
     assert_not tag.valid?
   end
+
+  test "Tag presence is true" do
+    tag = Tag.new(id: 1, tag_name: "")
+    tag.save
+    assert_not tag.valid?
+  end
+
+  test "Tag name has 1" do
+    @tag.tag_name = "n"
+    @tag.save
+    assert @tag.valid?
+  end
+
+  test "tag name has 15" do
+    @tag.tag_name = "n" * 15
+    @tag.save
+    assert @tag.valid?
+  end
+
+  test "Tag name has 16" do
+    @tag.tag_name = "n" * 16
+    @tag.save
+    assert @tag.valid?
+  end
+
 end
