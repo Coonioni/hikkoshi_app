779348cd1e723cad5bc99f6fa3e49caa958f4f7b
おみおみ <github-omiomi@example.com>
2022-04-04 13:09:02 +0900

    add comment-test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-
   has_many :notifications, dependent: :destroy
+
+  validates :user_id, :post_id, uniqueness: true
+  validates :content, presence: true, length: { maximum: 170 }
 end
